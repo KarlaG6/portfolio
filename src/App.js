f8650731afd0c0357c4b099b13cbdc70d6e95693
karlaG6 <karlaisabelgt@gmail.com>
2021-06-17 01:38:08 -0500

    lil improvements on theming and assets added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import {Fragment} from 'react';
-import { Grid } from '@material-ui/core';
+import theme from './theme.js';
+import { Grid, Container } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -11,6 +12,7 @@ import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { green, pink } from '@material-ui/core/colors';
+import {ReactComponent as Blob1} from './assets/blob.svg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,28 +24,16 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-}));
-const outerTheme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#93a0ef',
-      main: '#6272BD',
-      dark: '#2e478b',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#f2ffff',
-      main: '#BFF0CF',
-      dark: '#8ebd9e',
-      contrastText: '#fff'
-    },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
   },
-});
+}));
+
 
 function App() {
 
   const classes = useStyles();
-  const preventDefault = (event) => event.preventDefault();
+  // const preventDefault = (event) => event.preventDefault();
 
   const navOptions = [
     {title: 'Home', link: '#'},
@@ -56,27 +46,60 @@ function App() {
 
   return (
     <Fragment>
-      <ThemeProvider theme={outerTheme}>
+      <ThemeProvider theme={theme}>
         <AppBar position="static" >
           <Toolbar>
-            <Typography variant="h6" className={classes.title} color="inherit">
-                <Link 
-                  // component="button"
-                  color="inherit"
-                  href="#"
-                  // onClick={() => {
-                  //   alert("I'm a button.");
-                  // }}
-                  >
-                  Karla
-                </Link>
-              {navOptions.map(option => (
-                <Link color="inherit" href={option.link}> {option.title}</Link>
-              ))}
+            <Box flexGrow={1}><Button color="inherit">Karla</Button></Box>
+            <Box >
+              <Typography variant="h6" className={classes.title} color="inherit">
+              <Grid container spacing={3}>
+                {navOptions.map(option => (
+                  <Grid item>
+                    <Link color="inherit" href={option.link}> {option.title} </Link>
+                  </Grid>
+                ))}
+              </Grid>
             </Typography>
-            <Button color="inherit">Login</Button>
+              
+            </Box>
           </Toolbar>
         </AppBar>
+
+        <Container maxWidth="md" component="main" className={classes.heroContent}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>         
+            <Typography component="h2" variant="h2" color="textSecondary" gutterBottom>
+              <Box fontSize="h3.fontSize" color="textSecondary" fontWeight="fontWeightBold">
+              Hola, soy Karla
+              </Box>
+              <Box fontSize="h5.fontSize" color="textSecondary" fontWeight="fontWeightMedium">
+                Frontend developer
+              </Box>
+              <Box fontSize={19}  color="textSecondary">
+                Me apasiona aprender cosas nuevas en el ámbito de la tecnología, y como esto es una tarea de nunca acabar, he terminado por desarrollar considerablemente mi lado autodidacta, lo cual me llena de confianza para asumir nuevos retos.
+              </Box>
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Blob1 />
+            {/* <svg  viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" >
+    <mask id="mask0" mask-type="alpha">
+        <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
+        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
+        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
+        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
+    </mask>
+    <g mask="url(#mask0)">
+        <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
+        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
+        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
+        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
+        <image href="data:fotokarlablur.png" width="100px" height="100px"/>
+    </g>
+</svg> */}
+          </Grid>
+        </Grid>
+        </Container>
         
         <Box m={2} >
           <Grid container spacing={3}>
---
+++ src/theme.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#93a0ef',
+        main: '#6272BD',
+        dark: '#2e478b',
+        contrastText: '#fff'
+      },
+      secondary: {
+        light: '#f2ffff',
+        main: '#BFF0CF',
+        dark: '#8ebd9e',
+        contrastText: '#fff'
+      },
+    },
+    typography: {
+      fontFamily: `'Segoe UI', "Helvetica", "Arial", sans-serif`
+    }
+  });
+export default theme;
