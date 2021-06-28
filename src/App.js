801b70a40112e65ebee47959e655a5a5a5a931fe
karlaG6 <karlaisabelgt@gmail.com>
2021-06-28 03:57:27 -0500

    more content of Home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import {Fragment} from 'react';
 import theme from './theme.js';
-import { Grid, Container } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import { Grid, Container, Box, Paper, AppBar, Toolbar, Typography, Link } from '@material-ui/core';
+import { makeStyles, ThemeProvider, withStyles } from '@material-ui/core/styles';
+import { spacing } from '@material-ui/system';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { green, pink } from '@material-ui/core/colors';
 import {ReactComponent as Blob1} from './assets/blob.svg';
 import Img1 from './assets/Fig1';
+import foto from './assets/photo1.jpeg';
+import { Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,8 +27,23 @@ const useStyles = makeStyles((theme) => ({
   heroContent: {
     padding: theme.spacing(8, 0, 6),
   },
+  myimg: {
+    borderRadius: '8px',
+  },
 }));
 
+const BorderLinearProgress = withStyles((theme) => ({
+  root: {
+    height: 10,
+    borderRadius: 5,
+  },
+  colorPrimary: {
+    backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+  },
+  bar: {
+    borderRadius: 5,
+  },
+}))(LinearProgress);
 
 function App() {
 
@@ -65,41 +79,135 @@ function App() {
             </Box>
           </Toolbar>
         </AppBar>
+      {/* HELLO */}
+        <Box m={7} >
+          <Container maxWidth="md" component="main" className={classes.heroContent}>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>         
+                <Typography component="h2" variant="h2" color="textSecondary" alignCenter>
+                  <Box fontWeight="fontWeightBold" m={2} mx="auto">
+                  Hola, soy Karla
+                  </Box>
+                  <Box fontSize="h5.fontSize" color="textSecondary" fontWeight="fontWeightMedium" >
+                    Frontend developer
+                  </Box>
+                  <Box fontSize={19}  color="textSecondary" fontWeight="400" m={2} mx="auto">
+                    Una estudiante de ingeniería de sistemas con aspiración de crecer en el ámbito laboral como desarrolladora de software
+                  </Box>
+                </Typography>
+                <Button variant="contained" color="primary">Contact Me</Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Img1 />
+              </Grid>
+            </Grid>
+          </Container>
 
-        <Container maxWidth="md" component="main" className={classes.heroContent}>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>         
-            <Typography component="h2" variant="h2" color="textSecondary" alignCenter>
-              <Box fontSize="h3.fontSize" color="textSecondary" fontWeight="fontWeightBold" m={2} mx="auto">
-              Hola, soy Karla
+        {/* ABOUT ME */}
+          <Container maxWidth="md" className={classes.heroContent}>
+            <Typography component="h6" variant="h6" color="textSecondary"  alignCenter>
+              <Box fontSize="h3.fontSize"  fontWeight="fontWeightBold" textAlign="center">
+                About Me
               </Box>
-              <Box fontSize="h5.fontSize" color="textSecondary" fontWeight="fontWeightMedium" >
-                Frontend developer
-              </Box>
-              <Box fontSize={19}  color="textSecondary" fontWeight="400" m={2} mx="auto">
-                Me apasiona aprender cosas nuevas en el ámbito de la tecnología, y como esto es una tarea de nunca acabar, he terminado por desarrollar considerablemente mi lado autodidacta, lo cual me llena de confianza para asumir nuevos retos.
+              <Box textAlign="center" pb="1rem">
+                My Intro
               </Box>
             </Typography>
-            <Button variant="contained" color="primary">Contact Me</Button>
-          </Grid>
-          <Grid item xs={6}>
-            <Img1 />
-          </Grid>
-        </Grid>
-        </Container>
+            <Grid container spacing={7}>
+
+              <Grid item xs={6}>         
+                <img src={foto} width="100%" className={classes.myimg} alt="foto"/>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography component="h2" variant="h2" color="textSecondary" alignCenter>
+
+                  <Box fontSize={19} color="textSecondary" fontWeight="400" pb={1} mx="auto">
+                    Me apasiona aprender cosas nuevas en el ámbito de la tecnología, y como esto es una tarea de nunca acabar, he terminado por desarrollar considerablemente mi lado autodidacta, lo cual me llena de confianza para asumir nuevos retos.
+                  </Box>
+                </Typography>
+                <Box py={1} >
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} sm={4}>
+                      32+
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                      <Paper > xs=12 sm=4</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                      <Paper > xs=12 sm=4</Paper>
+                    </Grid>
+                  </Grid>
+                </Box>
+                <Button variant="contained" color="primary">Download CV</Button>
+              </Grid>
+            </Grid>
+          </Container>
         
-        <Box m={2} >
-          <Grid container spacing={3}>
-            <Grid item xs={12} m={2}>
-              <Paper  > xs=12</Paper>
+          {/* SKILLS */}
+          <Container maxWidth="md" className={classes.heroContent}>
+            <Typography component="h6" variant="h6" color="textSecondary"  alignCenter>
+              <Box fontSize="h3.fontSize"  fontWeight="fontWeightBold" textAlign="center">
+                Skills
+              </Box>
+              <Box textAlign="center" pb="1rem">
+                My technical level
+              </Box>
+            <Grid container spacing={5}>
+              <Grid item xs={6}>         
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Typography color="textSecondary">
+                      <Box fontSize="h5.fontSize"  fontWeight="fontWeightBold">
+                        Frontend developer
+                      </Box>
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className={classes.root}>
+                      <Box mb={1}>
+                        <Typography variant="body2" color="textSecondary" align="left">
+                          60%
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" align="right">
+                          70%
+                        </Typography>
+                      </Box>
+                      <BorderLinearProgress variant="determinate" value={70} />
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+
+              </Grid>
+
+              <Grid item xs={6}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="panel2a-header"
+                  >
+                    <Typography color="textSecondary">
+                      <Box fontSize="h5.fontSize"  fontWeight="fontWeightBold">
+                      Backend developer
+                      </Box>
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                      sit amet blandit leo lobortis eget.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper > xs=12 sm=6</Paper>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper > xs=12 sm=6</Paper>
-            </Grid>
-          </Grid>
+            </Typography>
+          </Container>
         </Box>
       </ThemeProvider>
     </Fragment>
