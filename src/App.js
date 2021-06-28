fc645949431547bb96bb37c91a4b826b71ee0b6d
karlaG6 <karlaisabelgt@gmail.com>
2021-06-27 20:21:01 -0500

    ya se muestra svg+img

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { green, pink } from '@material-ui/core/colors';
 import {ReactComponent as Blob1} from './assets/blob.svg';
+import Img1 from './assets/Fig1';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -68,35 +69,21 @@ function App() {
         <Container maxWidth="md" component="main" className={classes.heroContent}>
         <Grid container spacing={3}>
           <Grid item xs={6}>         
-            <Typography component="h2" variant="h2" color="textSecondary" gutterBottom>
-              <Box fontSize="h3.fontSize" color="textSecondary" fontWeight="fontWeightBold">
+            <Typography component="h2" variant="h2" color="textSecondary" alignCenter>
+              <Box fontSize="h3.fontSize" color="textSecondary" fontWeight="fontWeightBold" m={2} mx="auto">
               Hola, soy Karla
               </Box>
-              <Box fontSize="h5.fontSize" color="textSecondary" fontWeight="fontWeightMedium">
+              <Box fontSize="h5.fontSize" color="textSecondary" fontWeight="fontWeightMedium" >
                 Frontend developer
               </Box>
-              <Box fontSize={19}  color="textSecondary">
+              <Box fontSize={19}  color="textSecondary" fontWeight="400" m={2} mx="auto">
                 Me apasiona aprender cosas nuevas en el ámbito de la tecnología, y como esto es una tarea de nunca acabar, he terminado por desarrollar considerablemente mi lado autodidacta, lo cual me llena de confianza para asumir nuevos retos.
               </Box>
             </Typography>
+            <Button variant="contained" color="primary">Contact Me</Button>
           </Grid>
           <Grid item xs={6}>
-            <Blob1 />
-            {/* <svg  viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" >
-    <mask id="mask0" mask-type="alpha">
-        <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
-        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
-        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
-    </mask>
-    <g mask="url(#mask0)">
-        <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
-        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
-        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
-        <image href="data:fotokarlablur.png" width="100px" height="100px"/>
-    </g>
-</svg> */}
+            <Img1 />
           </Grid>
         </Grid>
         </Container>
